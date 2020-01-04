da55cf3989f28533caca73474fd542db10e98d03
laguarage <1754860514@qq.com>
2020-01-04 22:10:51 +0800

    Site updated: 2020-01-04 22:10:35

--- js/src/content_protect.js
+++ js/src/content_protect.js
@@ -1,40 +1,38 @@
 // 保护知识产权
-if(document.domain!='localhost'){
-    var copyright="";
-    // 如果在首页无法找到copyright的话
-    if(window.location.protocol + '//' + window.location.host+'/'!=window.document.location.href){
-        copyright=$(".copyright")[0]['innerText'];
-    } 
-    else{
-        copyright="文章作者:SaltedMdFiveSh\r\n\r\n作者链接:https://silver-birch-wawa.github.io/\r\n\r\n许可协议: '署名-非商用-相同方式共享 4.0' 转载请保留原文链接及作者。"
-		//copyright="Author:SaltedMdFiveSh\r\n\r\nAuthor Link:https://silver-birch-wawa.github.io/\r\n\r\n Agreement: 'Attribution-non-commercial-shared in the same way 4.0'\r\n\r\nReprinted please retain the original link and author."
-    }
-    $(document).bind({ 
-        copy: function(e) {//copy事件 
-			console.log('BIND COPY');
-            var cpTxt =copyright; 
+if (document.domain != 'localhost') {
+
+    $(document).bind({
+        copy: function(e) { //copy事件 
+            console.log('BIND COPY');
+            var copyRRight = "";
+            // 如果在首页无法找到copyRRight的话
+            if (window.location.protocol + '//' + window.location.host + '/' != window.document.location.href) {
+                copyRRight = $(".copyRRight")[0]['innerText'];
+            } else {
+                copyRRight = "文章作者:SaltedMdFiveSh\r\n\r\n作者链接:https://silver-birch-wawa.github.io/\r\n\r\n许可协议: '署名-非商用-相同方式共享 4.0' 转载请保留原文链接及作者。"
+                    //copyRRight="Author:SaltedMdFiveSh\r\n\r\nAuthor Link:https://silver-birch-wawa.github.io/\r\n\r\n Agreement: 'Attribution-non-commercial-shared in the same way 4.0'\r\n\r\nReprinted please retain the original link and author."
+            }
+            var cpTxt = copyRRight;
             var clipboardData = window.clipboardData; //for IE  
             if (!clipboardData) { // for chrome  
-                clipboardData = e.originalEvent.clipboardData;  
+                clipboardData = e.originalEvent.clipboardData;
             }
-            console.log("copy success.");
-        
+            console.log("copy success. copyRRight is " + cpTxt);
+
             //替换
             var selection = window.getSelection().toString();
-            if(selection.length<15){
-                clipboardData.setData('Text',selection); 
+            if (selection.length < 15) {
+                clipboardData.setData('Text', selection);
                 return false;
             }
-            if(e.clipboardData){
-                e.clipboardData.setData('text/plain', selection + copyright);
-            }else if(window.clipboardData){
+            if (e.clipboardData) {
+                e.clipboardData.setData('text/plain', selection + copyRRight);
+            } else if (window.clipboardData) {
                 //ie浏览器
-                window.clipboardData.setData('text/plain', selection + copyright);
-            } 
-            clipboardData.setData('Text',selection+'\r\n\r\n'+copyright);  
-            return false;//否则设不生效  
+                window.clipboardData.setData('text/plain', selection + copyRRight);
+            }
+            clipboardData.setData('Text', selection + '\r\n\r\n' + copyRRight);
+            return false; //否则设不生效  
         }
     });
 }
-
-
