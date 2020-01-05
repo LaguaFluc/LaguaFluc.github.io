634d35500884b0ec4a9cf0374cae3843922291e2
laguarage <1754860514@qq.com>
2020-01-05 12:44:39 +0800

    Site updated: 2020-01-05 12:44:27

--- js/src/content_protect.js
+++ js/src/content_protect.js
@@ -1,38 +1,35 @@
 // 保护知识产权
 if (document.domain != 'localhost') {
-
+    //复制事件发生时触发
     $(document).bind({
         copy: function(e) { //copy事件 
             console.log('BIND COPY');
-            var copyRRight = "";
-            // 如果在首页无法找到copyRRight的话
-            if (window.location.protocol + '//' + window.location.host + '/' != window.document.location.href) {
-                copyRRight = $(".copyRRight")[0]['innerText'];
-            } else {
-                copyRRight = "文章作者:SaltedMdFiveSh\r\n\r\n作者链接:https://silver-birch-wawa.github.io/\r\n\r\n许可协议: '署名-非商用-相同方式共享 4.0' 转载请保留原文链接及作者。"
-                    //copyRRight="Author:SaltedMdFiveSh\r\n\r\nAuthor Link:https://silver-birch-wawa.github.io/\r\n\r\n Agreement: 'Attribution-non-commercial-shared in the same way 4.0'\r\n\r\nReprinted please retain the original link and author."
-            }
-            var cpTxt = copyRRight;
+            //版权声明
+            copyright = "文章作者:SaltedMdFiveSh\r\n\r\n作者链接:https://silver-birch-wawa.github.io/\r\n\r\n许可协议: '署名-非商用-相同方式共享 4.0' 转载请保留原文链接及作者。"
+                //IE浏览器的粘贴板数据
             var clipboardData = window.clipboardData; //for IE  
+
             if (!clipboardData) { // for chrome  
+                //谷歌浏览器的粘贴板数据
                 clipboardData = e.originalEvent.clipboardData;
             }
-            console.log("copy success. copyRRight is " + cpTxt);
-
-            //替换
+            //选择的字符串
             var selection = window.getSelection().toString();
+            //字符串长度小于15则允许复制
             if (selection.length < 15) {
                 clipboardData.setData('Text', selection);
                 return false;
             }
+            //若有粘贴板数据
             if (e.clipboardData) {
-                e.clipboardData.setData('text/plain', selection + copyRRight);
+                //粘贴板数据将会变成 选择字符串 + 版权声明字符串
+                e.clipboardData.setData('text/plain', selection + copyright);
+                //ie浏览器情况的替换
             } else if (window.clipboardData) {
-                //ie浏览器
-                window.clipboardData.setData('text/plain', selection + copyRRight);
+                window.clipboardData.setData('text/plain', selection + copyright);
             }
-            clipboardData.setData('Text', selection + '\r\n\r\n' + copyRRight);
-            return false; //否则设不生效  
+            clipboardData.setData('Text', selection + '\r\n\r\n' + copyright);
+            return false;
         }
     });
 }
